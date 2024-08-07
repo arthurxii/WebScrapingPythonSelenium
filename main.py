c133406061c2c10ba483f2de74d76bcf6f3c4f92
arthurxii <arthur_drums@hotmail.com>
2024-08-07 19:41:57 -0300

    Abrindo o navegador e capturando o html da pagina

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# Iniciando o projeto
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options = Options()
+options.add_experimental_option("detach", True)
+options.add_argument('window-size=400,800')
+options.add_argument("--log-level=1")
+
+navegador = webdriver.Chrome(options=options)
+
+navegador.get("https://quotes.toscrape.com/")
+sleep(2)
+
+print(navegador.page_source)
+
+site = BeautifulSoup(navegador.page_source,'html.parser')
+
+print(site.prettify())
