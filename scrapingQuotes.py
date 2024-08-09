9ca67efee7e9d7c75eaea2dc6a76ba9ac43de7dd
arthurxii <arthur_drums@hotmail.com>
2024-08-09 11:31:53 -0300

    Fazendo o scraping da página quotes e salvando os dados em um arquivo csv

--- scrapingQuotes.py
+++ scrapingQuotes.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import pandas as pd
+import csv
 
 options = Options()
 options.add_experimental_option("detach", True)
-# options.add_argument('window-size=400,800')
 options.add_argument("--log-level=3")
 
 navegador = webdriver.Chrome(options=options)
@@ -18,29 +18,20 @@ sleep(2)
 page_content = navegador.page_source
 site = BeautifulSoup(page_content, 'html.parser')
 
-data_quotes = []
-
 text_places = site.find_all('div', attrs={'class': 'quote'})
 
-# print(text_place.prettify())
+data_quotes = []
 
 for text_place in text_places:
-    text_place_quote = text_place.find('span', attrs={'itemprop': 'text'})
-    text_place_author = text_place.find('small', attrs={'itemprop': 'author'})
+    text_place_quote = text_place.find('span', attrs={'itemprop': 'text'}).get_text(strip=True)
+    text_place_author = text_place.find('small', attrs={'itemprop': 'author'}).get_text(strip=True)
 
-    elements = [
-        (text_place_quote, "Citação"), 
-        (text_place_author, "Autor")
-    ]
-
-    for element, label in elements:
-        if element:
-            text = element.get_text(strip=True)
-            print(f"{label}: {text}")
-        else:
-            print("Elemento não encontrado")
+    # Para também exibir no terminal as citações e autores
+    print(f"Citação: {text_place_quote}")
+    print(f"Autor: {text_place_author}")
+    print("-" * 40)
     
     data_quotes.append([text_place_quote, text_place_author])
 
 dados = pd.DataFrame(data_quotes, columns=['Citação', 'Autor'])
-dados.to_csv('citacoes.csv', index=False)
+dados.to_csv('citacoes.csv', index=False, encoding="utf-8", quoting=csv.QUOTE_NONNUMERIC)
